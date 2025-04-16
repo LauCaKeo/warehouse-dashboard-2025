403fc79ecbdf7a5b74ca97650e62593340ac88f7
LauCaKeo <Phuc.cao@mia.vn>
2025-04-16 17:01:59 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import OrderSLAManagementSystem from "./components/OrderSLAManagementSystem";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container mx-auto p-4">
+      <OrderSLAManagementSystem />
+    </div>
+  );
+}
+
+export default App;
